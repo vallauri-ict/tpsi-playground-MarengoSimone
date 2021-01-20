7abe361ccd51abbbc5b6780e75533fa84cd8e785
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2021-01-20 13:08:38 +0100

    Aggiornato esercizio 3

--- Ajax/esercizio3 Ombrelloni/index.js
+++ Ajax/esercizio3 Ombrelloni/index.js
@@ -2,6 +2,8 @@
 const X_OFFSET = 180
 const Y_OFFSET = 210;
 const MMG = 24*3600*1000 // msec in un giorno
+const RIGHE = 18;
+const COLONNE = 37;
 
 $(document).ready(function(){	
 	
@@ -12,7 +14,54 @@ $(document).ready(function(){
 	let _dataInizio = $("#wrapper").find("input").eq(0)
 	let _dataFine = $("#wrapper").find("input").eq(1)
 	let _msg = $("#wrapper").children("label").eq(2)
+
+	_mappa.hide();
+	_btnVisualizzaMappa.prop({"disabled":true});
+	_dataFine.prop({"disabled":true});
+
+	_dataInizio.on("change",function(){
+		_dataFine.prop({"min":_dataInizio.val(),"disabled":false});
+	})
+
+	_dataFine.on("change",function(){
+		_btnVisualizzaMappa.prop("disabled",false);
+		_btnVisualizzaMappa.addClass("buttonEnabled");
+	})
+
+	_btnVisualizzaMappa.on("click",function(){
+		_mappa.show();
+		$.ajax({
+			"url":"http://localhost:3000/ombrelloni",
+			"error":errore,
+			"success":function(data){
+				console.log(data);
+				for(let i=0;i<=RIGHE;i++)
+				{
+					if(i!=9)
+					for(let j=0;j<=COLONNE;j++)
+					{
+						if(j!=22)
+						{
+							let div = $("<div>");
+							div.addClass("ombrellone");
+							div.appendTo(_mappa);
+							div.css({"top":X_OFFSET+(16*i),"left":Y_OFFSET+(16*j)})
+						}	
+					}
+				}
+			}
+		})
+	})
 		
  
 })
+
+function errore(jqXHR, textStatus, str_error){
+	if(jqXHR.status==0)
+	   alert("connection refused or server timeout");
+	else if (jqXHR.status == 200)
+	   alert("Errore Formattazione dati\n" + jqXHR.responseText);
+	else
+	   alert("Server Error: "+jqXHR.status+ " - " +jqXHR.responseText);
+	}
 
