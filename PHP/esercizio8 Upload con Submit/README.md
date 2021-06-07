1bdc6c63ab9ab27386e94d18bc030554614140c2
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2021-06-07 17:00:36 +0200

    Caricato esercizio 8: Upload con submit

---
+++ PHP/esercizio8 Upload con Submit/README.md
@@ -0,0 +1,8 @@
+8. > **LEZIONE 8**
+     8. > *Consegna PHP - Ese 8 Upload con submit*
+
+## Obiettivo:
+Tramite php è stato utilizzato il metodo Submit per caricare una file (dimensioni massime impostate da codice) all'interno della cartella **uploads**.
+
+## In corrispondenza del click sul pulsante submit:
+Viene generata la action che apre la pagina **upload.php**. Dopo aver aperto uno script PHP nel body della pagina, è stato caricato il file selezionato nella cartella e vengono visualizzate le informazioni del caricamento (**nome file, utente, estensione, dimensione e mimeType**).
---
+++ PHP/esercizio8 Upload con Submit/upload.php
@@ -0,0 +1,64 @@
+<!doctype  html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8"/> 
+	    <title> upload </title>
+	</head>
+
+	
+	<body>
+        <?php
+            // step 1
+            if(isset($_REQUEST["txtUser"]))
+                $txtUser = $_REQUEST["txtUser"];
+            else
+                die("Parametro mancante: txtUser");
+
+            if(isset($_FILES["txtFiles"]))
+                $fileRicevuti = $_FILES["txtFiles"];
+            else
+                die("Parametro mancante: txtFiles");
+
+            $overwrite = false;
+            if(isset($_REQUEST["chkOverwrite"]))
+                $overwrite = true;
+
+                $fileRicevuti=$_FILES["txtFiles"];
+                for ($i = 0; $i<count($fileRicevuti["name"]); $i++){
+                    $filename=basename($fileRicevuti["name"][$i]);
+                    $size=$fileRicevuti["size"][$i];
+                    if($size > 1000000)
+                    {
+                        echo("Il file $filename eccede le dimensioni massima di un MB e non può essere salvato sul server");
+                        continue; // forza all'inizio del ciclo, passa al prossimo file
+                    }
+                    $mimeType=$fileRicevuti["type"][$i];
+                    $ext = pathinfo($filename,PATHINFO_EXTENSION);
+                    $target_file = "uploads/$filename";
+                    if(file_exists($target_file))
+                    {
+                        if(!$overwrite)
+                        {
+                            echo("Il file $filename esiste già e non può essere sovrascritto");
+                            continue;
+                        }
+                    }
+                    if(move_uploaded_file($fileRicevuti["tmp_name"][$i],$target_file))
+                    {
+                        echo("File caricato correttamente </br>");
+                        echo("Name: $filename </br>");
+                        echo("Size: $size </br>");
+                        echo("Extension: $ext </br>");
+                        echo("mimeType: $mimeType </br>");
+                        echo("User: $txtUser </br> </br>");
+                    }
+                    else
+                    {
+                        echo("Errore nel caricamento del file $filename");
+                    }
+                }
+        
+        ?>
+	</body>
+	
+</html>
